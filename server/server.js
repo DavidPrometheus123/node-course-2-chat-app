e5d14f3330f942b569a27c9c677c92f340e2a16b
Ivan <korchaginivan6@gmail.com>
2019-02-05 18:52:08 +0000

    Second Push

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const path = require("path");
-const express = require("express");
+const express = require("express"),
+socketIO      = require("socket.io"),
+http          = require("http");
 
 const app = express();
+
+const server    = http.createServer(app);
+
+
+
+
+
+
 
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, process.env.IP, () => {
+server.listen(process.env.PORT, process.env.IP, () => {
    console.log("Server is listening"); 
 });
